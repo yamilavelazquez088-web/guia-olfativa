ef26a7f077c96f200b6fd47433f1a227c603cd40
yamilavelazquez088-web <yamilavelazquez088@gmail.com>
2026-09-18 22:35:45 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output:"standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
